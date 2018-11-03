a1e35a6a9e2695cf537bbee595f61d48e5440329
adams85 <adamosimoni@gmail.com>
2018-11-03 12:32:37 +0100

    import

---
+++ Calc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = Console.ReadLine();
+            int a = int.Parse(num1);
+
+            string num2 = Console.ReadLine();
+            int b = int.Parse(num2);
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
