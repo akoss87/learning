6fece9786314b8ce5dc6671b62097e1c6672a171
adams85 <adamosimoni@gmail.com>
2018-11-03 13:35:28 +0100

    ciklus

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Calc
 {
@@ -6,25 +7,39 @@ namespace Calc
     {
         static void Main(string[] args)
         {
-            string s1 = Console.ReadLine();
-            int a;
-            bool success = int.TryParse(s1, out a);
-            if (!success)
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int a, b;
+            Console.WriteLine("Adja meg az első számot:");
+
+            do
             {
-                Console.WriteLine("Hiba: Csak egész számok adhatóak meg.");
-                return;
+                string s1 = Console.ReadLine();
+                bool success = int.TryParse(s1, out a);
+
+                if (success)
+                    break;
+                else
+                    Console.WriteLine("Hiba: Csak egész számok adhatóak meg.");
             }
+            while (true);
+
+            Console.WriteLine("Adja meg a másik számot:");
+
+            do
+            {
+                string s2 = Console.ReadLine();
+                bool success = int.TryParse(s2, out b);
+                if (success)
+                    break;
+                else
+                    Console.WriteLine("Hiba: Csak egész számok adhatóak meg.");
+
+            }
+            while (true);
 
 
-            string s2 = Console.ReadLine();
-            int b;
-            success = int.TryParse(s2, out b);
-            if (!success)
-            {
-                Console.WriteLine("Hiba: Csak egész számok adhatóak meg.");
-                return;
-            }
-                        
             Console.WriteLine(a + b);
         }
     }
