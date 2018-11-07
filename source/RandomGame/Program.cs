c7c4db92d40dab21d9de3b60ba32af3657021773
simonakos87 <simonakos87@gmail.com>
2018-11-07 11:55:30 +0100

    randomgame program

---
+++ source/RandomGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace RandomGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var random = new Random();
+            int randomNumber = random.Next(100);
+            Console.WriteLine("Gondoltam egy számra 0 és 99 között.");
+            Console.WriteLine("Találd ki, melyik az: ");
+
+            while (true)
+            {
+                string numString = Console.ReadLine();
+                int numInt = 0;
+                if (int.TryParse(numString, out numInt))
+                {
+                    if (numInt < randomNumber)
+                        Console.WriteLine("Nem jó. Kisebb a megadott szám, mint a kigondolt.");
+                    else if (numInt > randomNumber)
+                        Console.WriteLine("Nem jó. Nagyobb a megadott szám, mint a kigondolt.");
+                    else
+                    {
+                        Console.WriteLine("Eltaláltad a kigondolt számot.");
+                        break;
+                    }
+                }
+                else
+                    Console.WriteLine("Hibás számot adtál meg.");
+            }
+        }
+    }
+}
