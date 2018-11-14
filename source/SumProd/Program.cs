217cc09851597e47fbbfc84154544ca7e1307d38
adams85 <adamosimoni@gmail.com>
2018-11-14 10:46:29 +0100

    vezérlési szerkezetek + SumProd projekt

---
+++ source/SumProd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumProd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
