34d8ff0757a88aef399ed8188922974d3adae3eb
Adam Simon <adamosimoni@gmail.com>
2018-12-29 12:34:16 +0100

    kerekítések

---
+++ source/SampleLibrary/IntUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleLibrary
+{
+    public static class IntUtils
+    {
+        public static int ToIntSafe(double x)
+        {
+            return checked((int)x);
+        }
+
+        public static int RoundToTens(int value)
+        {
+            //return checked((int)Math.Round(x/10.0, MidpointRounding.AwayFromZero) * 10);
+
+            value = Math.DivRem(value, 10, out var lastDigit);
+
+            if (lastDigit >= 5)
+                value++;
+            else if (lastDigit <= -5)
+                value--;
+
+            checked { value *= 10; }
+
+            return value;
+        }
+    }
+}
